b69a375c29aae1f4fd8b9c0406a7eb580d68fa31
Nick Ivanukov <ivanukov78@gmail.com>
2024-02-12 18:21:47 +0400

    create main file

---
+++ src/__main__.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+import uvicorn
+from src.api.v1 import api
+
+app = FastAPI()
+
+app.include_router(api.router)
+
+
+def main() -> None:
+    uvicorn.run("main:app", host='127.0.0.1', port=8000)
+
+
+if __name__ == "__main__":
+    main()
