5281227bd5ac05bdd020c3c64134c8b4dff8dbdb
Nick Ivanukov <ivanukov78@gmail.com>
2024-02-12 17:24:02 +0400

    create sql model

---
+++ src/service/database/models/__init__.py
@@ -0,0 +1,4 @@
+from src.service.database.models.base import Base
+from src.service.database.models.user import User
+
+__all__ = ("Base", "User")
---
+++ src/service/database/models/user.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+from src.service.database.models.base import Base
+
+
+class User(Base):
+    id: Mapped[int] = mapped_column(index=True, primary_key=True)
+    email: Mapped[str] = mapped_column(nullable=False, unique=True)
+    hashed_password: Mapped[str] = mapped_column(nullable=False)
+    is_active: Mapped[bool] = mapped_column(default=False, nullable=False)
